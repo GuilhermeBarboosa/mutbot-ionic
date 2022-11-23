b43e2769fcd8febf896d12596badbb6c99b3cec3
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-23 15:45:06 -0300

    FIX: created rotine implements like values

--- src/app/shared/components/cards-questions/cards-questions.component.ts
+++ src/app/shared/components/cards-questions/cards-questions.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards-questions',
   templateUrl: './cards-questions.component.html',
-  styleUrls: ['./cards-questions.component.css']
+  styleUrls: ['./cards-questions.component.css'],
 })
 export class CardsQuestionsComponent implements OnInit {
+  @Input() valor = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  plusLike() {
+    this.valor++;
+    this.likeColor(this.valor);
   }
 
+  lessLike() {
+    this.valor--;
+    this.likeColor(this.valor);
+  }
+
+  likeColor(valor: number): void {
+    if (valor <= 0) {
+      console.log(valor);
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 }
