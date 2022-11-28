0cf3202edae5c25e71a1a3467c7868b659909d1c
joao.floriano <joao.floriano@sankhya.com.br>
2022-11-28 18:03:15 -0300

    resposta

--- src/app/shared/components/card-question-list/card-question-list.component.ts
+++ src/app/shared/components/card-question-list/card-question-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { InfiniteScrollCustomEvent, ModalController, NavController, NavParams } from '@ionic/angular';
+import { Subject, takeUntil } from 'rxjs';
+import { ListParams } from '../../interfaces/list-params';
 import { Question } from '../../interfaces/question';
+import { MessageService } from '../../services/messages.service';
+import { ResponseService } from '../../services/response.service';
+import { CardResponseForm } from '../card-response-form/card-response-form.page';
 
 @Component({
   selector: 'card-questions-list',
@@ -11,13 +16,54 @@ export class CardQuestionsListComponent implements OnInit {
   
   question: Question
 
-  item = ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ', 'aaaa', 'aaa'];
-  constructor(private modalCtrl: ModalController, public navParams: NavParams) { 
+  responseArray = [];
+
+  private unsubscribeNotifier = new Subject<void>();
+  params: ListParams = {
+    pageable: true,
+    pageIndex: 0,
+    pageSize: 10,
+    orderBy: 'response.createdDate'
+  };
+
+  pageIndex: number = 0;
+  constructor(private modalCtrl: ModalController,
+     public navParams: NavParams,
+     private responseService: ResponseService,
+     private messageService: MessageService) { 
     this.question = navParams.get('question');
     console.log(this.question)
   }
 
   ngOnInit() {
+    this.loadResponse(0);
+  }
+
+  loadResponse(index) {
+    this.params.pageIndex = index;
+    console.log(index)
+    this.responseService.load(this.params, this.question.id).pipe(
+      takeUntil(this.unsubscribeNotifier),
+    ).subscribe({
+      next: data => {       
+          this.responseArray = this.responseArray.concat(data.data);        
+      },
+      error: () =>this.messageService.error('Falha ao carregar Questões!')
+    });
+  }
+
+  async openModalResponse() {
+    const modal = await this.modalCtrl.create({
+      component: CardResponseForm, componentProps: { question: this.question}
+    });
+
+    modal.onDidDismiss()
+      .then(()=> {
+        this.pageIndex = 0;
+        this.responseArray = [];
+        this.loadResponse(this.pageIndex);
+      })
+    return await modal.present();
   }
 
   dismiss() {
@@ -25,8 +71,8 @@ export class CardQuestionsListComponent implements OnInit {
   }
 
   onIonInfinite(ev) {
-    // this.pageIndex = (this.pageIndex + 1);
-    // this.loadQuestions(this.pageIndex);
+    this.pageIndex = (this.pageIndex + 1);
+    this.loadResponse(this.pageIndex);
     setTimeout(() => {
       (ev as InfiniteScrollCustomEvent).target.complete();
     }, 500);
